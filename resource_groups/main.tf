76201f2f3ce6d516136bbf209f5497b4293bae32
Kevin Delaney <thewelshvader@hotmail.com>
2024-02-29 13:23:47 +0000

    correcting provider

--- resource_groups/main.tf
+++ resource_groups/main.tf
@@ -1,8 +1,11 @@
 terraform {
-  provider {
+  required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
   }
+}
+provider "azurerm" {
+    features {}
 }
