85f5b712654265eac44bc40963a5d50a07e8182f
Kevin Delaney <thewelshvader@hotmail.com>
2024-02-29 13:05:38 +0000

    testing init

--- backend.tf
+++ backend.tf
@@ -3,6 +3,5 @@ terraform {
     backend "azurerm {
         resource_group_name = "rsg-storage_accounts"
         storage_account_name = "gdalestorageacc01"
-        container_name = "gdalestoragecontainer01"
     }
 }
