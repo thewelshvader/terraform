d4232aec3c9320c6faf501cf2b5a9024a9bc4d68
Kevin Delaney <thewelshvader@hotmail.com>
2024-03-01 10:11:21 +0000

    testing tfvars

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+# Resource Groups
+rsg_rancher       = "rsg-rancher"
+rsg_network       = "rsg-network"
+ 
--- vars.tf
+++
@@ -1,9 +0,0 @@
-# Resource Groups
-variable "rsg_rancher" {
-  type = string
-  default = "placeholder"
-}
-variable "rsg_network" {
-  type = string
-  default = "placeholder"
-}
